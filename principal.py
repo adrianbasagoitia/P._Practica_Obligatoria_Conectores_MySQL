0f44a74cc65ecdb0b100a0584f326f90a119c5ac
adrianbasagoitia <79417614+adrianbasagoitia@users.noreply.github.com>
2024-11-28 10:50:12 +0100

    Implementacion del metodo comprobar_instalacion_pymysql

--- principal.py
+++ principal.py
@@ -38,7 +38,40 @@ def iniciar_programa():
 
     print("\n"*2+"Terminando ejecucion del programa."+"\n"+"#"*60, end=" ")
 
+
 # ######################################################################### #
+def comprobar_instalacion_pymyqsl():
+  """
+  Comprueba si la libreria PyMySQL esta instalada en el sistema.
+
+  Para comprobar la existencia de la libreria, se intenta realizar un import
+  de la misma, gestionandolo con un try / except. En caso de que la libreria
+  no este instalada se genera una excepcion y se cambia el valor de una
+  variable booleana a False. Si resulta estar instalada, se cambia el valor
+  de la variable booleana a True.
+
+  No se puede evitar el lanzamiento de la exception.
+
+  Returns:
+      bool: True, Si la libreria lxml esta instalada, False, en cualquier 
+      otro caso.
+  """
+  # Local variables
+  instalado:bool = False # Si la libreria PyMySQL esta instalada su valor sera
+  # True, en cualquier otro caso, sera False.
+
+
+  # Local code
+  try: # Intentar importar la libreria
+    import pymysql
+    # Si se llega hasta aqui, la libreria esta instalada
+    instalado = True
+  
+  except ModuleNotFoundError as e: # Si la libreria no esta instalada, se lanza
+    # una excepcion, y se cambia el valor a False.
+    instalado = False
+  
+  return instalado
 
 
 # ############################################################################ #
