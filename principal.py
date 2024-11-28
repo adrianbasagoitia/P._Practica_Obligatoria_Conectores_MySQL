602664a50008b74788e918a4c0fc0cc4cc793833
adrianbasagoitia <79417614+adrianbasagoitia@users.noreply.github.com>
2024-11-28 10:37:45 +0100

    Implementacion parcial de iniciar_programa

--- principal.py
+++ principal.py
@@ -9,7 +9,34 @@
 
 
 # ################################ Methods ################################ #
+def iniciar_programa():
+  # Local variables
+  conector_presente:bool = False # Contiene si la libreria pymysql esta 
+  # presente en el sistema.
 
+
+  # Local code
+  print("\n"*2+"#"*60)
+  print("Inicializando programa...")
+
+  # Comprobar instalacion de pymsql
+  print("Comprobando instalacion del conector PyMySQL...")
+  conector_presente = comprobar_instalacion_pymyqsl()
+
+  if(conector_presente):
+    print("\nConector PyMySQL presente en el sistema.")
+  
+  else:
+    print("El conector PyMySQL no esta presente en el sistema.")
+    print("Sin un conector a la base de datos, el programa no puede continuar")
+    print("con la ejecucion.")
+    print("\nPuede instalar el conector a traves de pip con el siguiente comando:")
+    print("pip install PyMySQL")
+    print("o puede consultar otras formas de instalacion en la documentacion")
+    print("oficial del conector:")
+    print("https://pymysql.readthedocs.io/en/latest/")
+
+    print("\n"*2+"Terminando ejecucion del programa."+"\n"+"#"*60, end=" ")
 
 # ######################################################################### #
 
