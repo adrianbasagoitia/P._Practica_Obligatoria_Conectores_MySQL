977c3f4192422b1a5e24289ef3b541cb4dc00bab
adrianbasagoitia <79417614+adrianbasagoitia@users.noreply.github.com>
2024-11-30 20:34:41 +0100

    Implementacion del metodo query_select.
    Modificacion del metodo query_update. Modificacion de la documentacion y anaydir una condicion en la clausula where.

--- base_datos.py
+++ base_datos.py
@@ -787,7 +787,6 @@ def query_update(nombre_tabla:str, set:list[tuple], join:list[tuple] = None, whe
       nombre_tabla (str): 
         Nombre de la tabla donde se insertaran las nuevas filas.
 
-
       set (list[tuple]):
         Lista de tuplas, cada tupla es un set, que se debe anyadir a la
         consulta. Cada tupla tiene 3 posisicones:
@@ -890,8 +889,178 @@ def query_update(nombre_tabla:str, set:list[tuple], join:list[tuple] = None, whe
 
     if(i != len(where)-1): # Si no es la ultima clausula, anyadir el AND
       query += " AND\n"
+
+    else: # Si el la ultima linea, solo anyadir el salto de linea
+        query += "\n"
   
   # Al final de la consulta, anyadir el punto y coma
   query += ";"
 
   return (0, "Query para generar update escrita", query)
+
+
+# ######################################################################### #
+def query_select(nombre_tabla:str, columnas:list[tuple] = None, join:list[tuple] = None, where:list[tuple] = None, order_by:list[tuple] = None, limit:str = None):
+  """
+  Crea una query para seleccionar filas de una tabla.
+
+  Es un metodo interno al programa, el usuario nunca lo tocara o modificara.
+  Por lo tanto, no se verifica la validez de los parametros introducidos.
+  Se asume que siempre son correctos.
+
+  Args:
+      nombre_tabla (str): 
+        Nombre de la tabla donde se insertaran las nuevas filas.
+
+      columnas (list[tuple], optional): 
+        Lista de tuplas, cada tupla es una columna de las tablas, que se
+        debe anyadir a la consulta, consta de 2 posiciones:
+          - Nombre tabla (str):
+            Tabla de la que obtener la columna.
+          
+          - Nombre columna (str):
+            Columna a anyadir a la consulta.
+        Este es un parametro opcional. Su valor por defecto es None. Si no se
+        indica ninguna columna, se seleccionaran todas las posibles, utilizando
+        * en la query.
+
+      join (list[tuple], optional):
+        Lista de tuplas, cada tupla es un join, que se debe anyadir a la 
+        consulta. Cada tupla, consta de 4 posiciones:
+          - Tipo de join (str):
+            Tipo  de join a anadir a la consulta.
+
+          - Nombre de la tabla (str):
+            Nombre de la tabla sobre la que hacer el join.
+          
+          - Campo tabla original (str):
+            Campo de la tabla original por el cual hacer el join.
+          
+          - Campo tabla join (str):
+            Campo de la tabla anyadida por el cual hacer el join.
+        Este es un parametro opcional. Su valor por defectos es None.
+
+      where (list[tuple], optional):
+        Lista de tuplas, cada tupla es una condicion que se debe anyadir al
+        where. Cada tupla consta de cuatro posiciones:
+          - Nombre de la tabla (str):
+            Tabla sobre la que se comprobara la condicion.
+
+          - Nombre del campo (str):
+            Nombre del campo sobre el cual aplicar la condicion.
+          
+          - Condicion (str):
+            Condicion a aplicar sobre el campo, solo se acepta <, > o =.
+          
+          - Valor de la condicion (str):
+            Valor a aplicar con la condicion sobre el campo. Si es una
+            cadena de caracteres se deben introducir " o '.
+        Este es un parametro opcional. Su valor por defectos es None.
+
+      order_by (list[tuple], optional):
+        Lista de tuplas, cada tupla es una columna por la que ordenar el
+        resultado de la query. Cada tupla consta de 3 posiciones:
+          - Nombre tabla (str):
+            Tabla de la que obtener la columna por la que ordenar el resultado.
+          
+          - Nombre columna (str):
+            Columna por la que ordenar el resultado.
+          
+          - Tipo orden (str):
+            Orden Ascencente (ASC) o descendene (DESC).
+
+      limit (str, optional):
+        Indica el numero de filas maximo a devolver. Se maneja como una
+        cadena de caracteres por simplicidad. Opcional. Valor por defecto
+        None.
+    
+  Returns:
+      tuple: tres posiciones:
+        - codigo de resultado (int): 
+          0 en caso de ejecucion correcta, -1 en cualquier otro caso.
+        - mensaje de ejecucion (str): 
+          Mensaje para el usuario informando del resultado de la ejecucion 
+          del metodo.
+        - query (str): 
+          Cadena de caracteres conteniendo la query para ejecutar sobre la 
+          conexion al servidor de la base de datos.
+  """  
+  # Local variables
+  query = "SELECT "
+
+
+  # Local code
+  # ##### Columnas #####
+  if(columnas is None): # Si no hay columnas
+    query += "*" # Se seleccionan todos los campos
+  
+  else: # Se han especificado las columnas a incluir
+    # Estructurra de la tupla de una columna
+    # Nombre tabla | Nombre columna
+    for i in range(0, len(columnas)): # Recorrer las tuplas que contienen las 
+      # columnas.
+      query += f"{columnas[i][0].upper()}.{columnas[i][1].upper()}" # Nombre_campo.
+      # Nombre_columna
+      
+      if(i != len(columnas)-1): # Si no es la ultima columna, separar por una 
+        # coma y un espacio en blanco
+        query += ", "
+
+      else: # Si es la ultima columna, salto de linea
+        query += "\n"
+  
+
+  # ##### Nombre tabla #####
+  query += f"\tFROM {nombre_tabla.upper()}\n"
+
+  # ##### Join #####
+  if(join is not None): # Si hay que hacer algun Join
+    # Estructura de la tupla de un join
+    # Tipo Join (Inner, Left, Right), Nombre Tabla | nombre_campo_tabla |
+    # nombre_campo_tabla_join
+    for i in range(0, len(join)):
+      query += f"\t\t{join[i][0].upper()} {join[i][1].upper()}\n\t\t\tON {nombre_tabla.upper()}.{join[i][2].upper()} = {join[i][1].upper()}.{join[i][3].upper()}\n"
+      # Por cada join una linea. Tipo de Join nombre_tabla ON 
+      # nombre_tabla_original.campo = nombre_tabla.campo
+      # Ej INNER JOIN DEPARTAMENTO ON EMPLEADO.DEPARTAMENTO = DEPARTAMENTO.ID 
+  
+
+    ##### Where #####
+    query += "\tWHERE\n" # Anyadir el where
+
+    # Estructura de la tupla de un where
+    # nombre_tabla | nombre_campo | condicion (<, >, =) | valor |
+    for i in range(0, len(where)): # Recorrer la lista de condiciones del where
+      # Anyadir el where
+      query += f"\t\t{where[i][0].upper()}.{where[i][1].upper()} {where[i][2].upper()} {where[i][3].upper()}"
+
+      if(i != len(where)-1): # Si no es la ultima clausula, anyadir el AND
+        query += " AND\n"
+      
+      else: # Si el la ultima linea, solo anyadir el salto de linea
+        query += "\n"
+    
+    # ##### Order By #####
+    # Estructura de una tupla de order by
+    # Nombre Tabla | Nombre columna | ASCendente o DESCendente
+    if(order_by is not None): # Si la query tiene que ordenarse de alguna manera
+      # Escribir Order By
+      query += "\tORDER BY\n"
+      for i in range(0, len(order_by)):
+        query += f"\t\t{order_by[i][0].upper()}.{order_by[i][1].upper()} {order_by[i][2].upper()}"
+
+        if(i != len(order_by)-1): # Si no es la ultima columna por la que 
+          # ordenar, anyadir una coma
+          query += ","
+
+        # Siempre al final de cada linea de order by anyadir un salto de linea
+        query += "\n"
+    
+    # ##### Limit #####
+    if(limit is not None):
+      query += f"\tLIMIT {limit}"
+    
+    # Al final de la query, siempre anyadir punto y coma
+    query += ";"
+
+  return (0, "Query para generar select escrita", query)
